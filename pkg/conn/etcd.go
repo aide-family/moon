2f5169117629c9d1f6ec08592027466f73b1ef9e
梧桐 <1058165620@qq.com>
2023-08-16 13:09:15 +0800

    style: [master|node] server registrar

--- pkg/conn/etcd.go
+++ pkg/conn/etcd.go
@@ -1,17 +1,22 @@
 package conn
 
 import (
-	"context"
+	"sync"
+
 	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
-	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/registry"
 	clientV3 "go.etcd.io/etcd/client/v3"
-	gGrpc "google.golang.org/grpc"
-	"time"
 )
 
 type EtcdConfig interface {
 	GetEndpoints() []string
 }
+
+var (
+	dis  registry.Discovery
+	reg  *etcd.Registry
+	once sync.Once
+)
 
 // NewETCDClient new etcd client
 //
@@ -36,20 +41,24 @@ func NewETCDClient(conf EtcdConfig) *clientV3.Client {
 //
 //	@param etcdClient *clientV3.Client
 func NewETCDRegistrar(etcdClient *clientV3.Client) *etcd.Registry {
-	return etcd.New(etcdClient)
+	registry := etcd.New(etcdClient)
+	once.Do(func() {
+		dis = registry
+		reg = registry
+	})
+	return registry
 }
 
-func GetGRPCConn(client *clientV3.Client, serverName string) (*gGrpc.ClientConn, error) {
-	ctx, cel := context.WithTimeout(context.Background(), time.Second*60)
-	dis := etcd.New(client, etcd.Context(ctx))
-	endpoint := "discovery:///" + serverName
+func GetDiscovery() registry.Discovery {
+	if dis == nil {
+		panic("discovery is nil")
+	}
+	return dis
+}
 
-	defer cel()
-	conn, err := grpc.DialInsecure(
-		ctx,
-		grpc.WithEndpoint(endpoint),
-		grpc.WithDiscovery(dis),
-		grpc.WithTimeout(60*time.Second),
-	)
-	return conn, err
+func GetRegistrar() *etcd.Registry {
+	if reg == nil {
+		panic("registrar is nil")
+	}
+	return reg
 }
