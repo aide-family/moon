37adc08a4769fab380fbc32121b9f20420b8c4d6
梧桐 <1058165620@qq.com>
2023-12-27 14:16:38 +0800

    feat: 策略变更, 维护策略组的策略开启状态数量字段

--- pkg/helper/model/prom_strategy_groups.go
+++ pkg/helper/model/prom_strategy_groups.go
@@ -11,12 +11,13 @@ const TableNamePromGroup = "prom_strategy_groups"
 // PromStrategyGroup 策略组
 type PromStrategyGroup struct {
 	query.BaseModel
-	Name           string          `gorm:"column:name;type:varchar(64);not null;comment:规则组名称;index:idx__name,unique"`
-	StrategyCount  int64           `gorm:"column:strategy_count;type:bigint;not null;comment:规则数量"`
-	Status         valueobj.Status `gorm:"column:status;type:tinyint;not null;default:1;comment:启用状态1:启用;2禁用"`
-	Remark         string          `gorm:"column:remark;type:varchar(255);not null;comment:描述信息"`
-	PromStrategies []*PromStrategy `gorm:"foreignKey:GroupID"`
-	Categories     []*PromDict     `gorm:"many2many:prom_group_categories"`
+	Name                string          `gorm:"column:name;type:varchar(64);not null;comment:规则组名称;index:idx__name,unique"`
+	StrategyCount       int64           `gorm:"column:strategy_count;type:bigint;not null;comment:规则数量"`
+	EnableStrategyCount int64           `gorm:"column:enable_strategy_count;type:bigint;not null;comment:启用策略数量"`
+	Status              valueobj.Status `gorm:"column:status;type:tinyint;not null;default:1;comment:启用状态1:启用;2禁用"`
+	Remark              string          `gorm:"column:remark;type:varchar(255);not null;comment:描述信息"`
+	PromStrategies      []*PromStrategy `gorm:"foreignKey:GroupID"`
+	Categories          []*PromDict     `gorm:"many2many:prom_group_categories"`
 }
 
 // TableName PromStrategyGroup's table name
