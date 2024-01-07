431bf7f29fdf48a5848c6c1490f76fa9ddbb74d1
梧桐 <1058165620@qq.com>
2024-01-07 18:09:51 +0800

    feat: 调整API

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // const assetApi = 'http://192.168.10.2:8002/'
-const assetApi = 'http://localhost:8002/'
+// const assetApi = 'http://localhost:8002/'
 // const assetApi = 'http://124.223.104.203:8802/'
-// const assetApi = 'https://prometheus.aide-cloud.cn/'
+const assetApi = 'https://prometheus.aide-cloud.cn/'
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
