d092adcd718812d63eaefb75aa9ec905dc870f9a
梧桐 <aidecloud@163.com>
2024-02-27 13:03:56 +0800

    feat: 初始化api数据
    feat: 初始化api数据

--- app/prom_server/internal/biz/do/migrate.go
+++ app/prom_server/internal/biz/do/migrate.go
@@ -1,11 +1,13 @@
 package do
 
 import (
+	_ "embed"
+
 	"gorm.io/gorm"
 )
 
-func Migrate(db *gorm.DB) error {
-	return db.AutoMigrate(
+func Migrate(db *gorm.DB) (err error) {
+	err = db.AutoMigrate(
 		&PromAlarmChatGroup{},
 		&PromAlarmHistory{},
 		&PromAlarmIntervene{},
@@ -30,4 +32,19 @@ func Migrate(db *gorm.DB) error {
 		&MyChart{},
 		&MyDashboardConfig{},
 	)
+	if err != nil {
+		return err
+	}
+	return initSysApi(db)
+}
+
+//go:embed init.sql
+var sql string
+
+// InitSysApi 初始化系统接口权限列表
+func initSysApi(db *gorm.DB) (err error) {
+	if err = db.Model(&SysAPI{}).Unscoped().Where("id > 0").Delete(&SysAPI{}).Error; err != nil {
+		return err
+	}
+	return db.Exec(sql).Error
 }
