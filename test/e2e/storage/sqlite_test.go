b492f0f38616b15d0b60d91bfa2e5a9b4590b3a6
tyvekzhang <tyvekzhang@gmail.com>
2024-02-28 17:16:08 +0800

    Feat: e2e test file ready for sqlite fix typo

---
+++ test/e2e/storage/sqlite_test.go
@@ -0,0 +1,93 @@
+package e2e
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price int
+}
+
+type Config struct {
+	Data struct {
+		Database struct {
+			Driver string `yaml:"driver"`
+			Source string `yaml:"source"`
+			Debug  bool   `yaml:"debug"`
+		} `yaml:"database"`
+	} `yaml:"data"`
+}
+
+func TestSqlite(t *testing.T) {
+	exePath, err := os.Getwd()
+	assert.NoError(t, err)
+	configDir := filepath.Dir(filepath.Dir(filepath.Dir(exePath)))
+	configPath := filepath.Join(configDir, "test/fixtures/testdata/storage/config.yaml")
+	t.Logf("ConfigPath: %s", configPath)
+
+	yamlFile, err := os.ReadFile(configPath)
+	assert.NoError(t, err)
+
+	var conf Config
+	err = yaml.Unmarshal(yamlFile, &conf)
+	assert.NoError(t, err)
+
+	driver := conf.Data.Database.Driver
+	source := conf.Data.Database.Source
+	debug := conf.Data.Database.Debug
+	t.Logf("Driver: %s", driver)
+	t.Logf("Source: %s", source)
+	t.Logf("Debug: %v", debug)
+
+	if driver != "sqlite" {
+		t.Skip("Skipping test. Only SQLite driver is supported.")
+	}
+
+	var opts []gorm.Option
+	var dialector gorm.Dialector
+	dialector = sqlite.Open(source)
+	db, err := gorm.Open(dialector, opts...)
+	assert.NoError(t, err)
+	if debug {
+		db = db.Debug()
+	}
+
+	err = db.AutoMigrate(&Product{})
+	assert.NoError(t, err)
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	var product Product
+	db.First(&product, 1)
+	assert.Equal(t, "D42", product.Code)
+	assert.Equal(t, 100, product.Price)
+
+	db.First(&product, "code = ?", "D42")
+	assert.Equal(t, "D42", product.Code)
+	assert.Equal(t, 100, product.Price)
+
+	db.Model(&product).Update("Price", 200)
+	db.First(&product, 1)
+	assert.Equal(t, "D42", product.Code)
+	assert.Equal(t, 200, product.Price)
+
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	db.First(&product, 1)
+	assert.Equal(t, "F42", product.Code)
+	assert.Equal(t, 200, product.Price)
+
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	db.First(&product, 1)
+	assert.Equal(t, "F42", product.Code)
+	assert.Equal(t, 200, product.Price)
+
+	db.Delete(&product, 1)
+	defer os.Remove(source)
+}
