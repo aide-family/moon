f865c1505d7246b4ef28c46b03fb24920143e665
梧桐 <aidecloud@163.com>
2024-03-02 19:33:28 +0800

    bugfix: 解决docker环境一起拉起异常问题

--- app/prom_agent/internal/server/watch.go
+++ app/prom_agent/internal/server/watch.go
@@ -159,6 +159,7 @@ func (w *Watch) onlineNotify() error {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	time.Sleep(2 * time.Second)
 	return w.interflowInstance.Send(ctx, serverUrl, msg)
 }
 
