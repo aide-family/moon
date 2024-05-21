5d6cdf40c7e9373f8070300a93228efbc1f6cc7c
梧桐 <1058165620@qq.com>
2024-05-21 15:58:55 +0800

    feat: 优化action

--- build_images.sh
+++ build_images.sh
@@ -7,13 +7,13 @@ executables=("palace" "rabbit")
 # 遍历每个可执行文件并构建镜像
 for exec in "${executables[@]}"; do
   # 设置镜像名称，可以根据需要修改
-  image_name="aidemoonio/${exec}:$VERSION"
+  image_name="$1/${exec}:$VERSION"
   # 构建 Docker 镜像
   docker build --build-arg CMD_PARAMS=${exec} -t ${image_name} .
   # 打一个latest版本
-  docker tag ${image_name} "aidemoonio/${exec}:latest"
+  docker tag ${image_name} "$1/${exec}:latest"
   # 推送到 Docker 仓库
   docker push ${image_name}
-  docker push "aidemoonio/${exec}:latest"
+  docker push "$1/${exec}:latest"
   echo "Built ${image_name} for executable ${exec}"
 done
