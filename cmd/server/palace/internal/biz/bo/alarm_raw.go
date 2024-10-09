06ec53a08fe774c7f5a224c0565cf483d5f83129
jinronga <1460595002@qq.com>
2024-10-09 16:37:44 +0800

    feat:Increase storage for historical and real-time alarm information

--- cmd/server/palace/internal/biz/bo/alarm_raw.go
+++ cmd/server/palace/internal/biz/bo/alarm_raw.go
@@ -8,4 +8,22 @@ type (
 		RawInfo     string `json:"rawInfo"`
 		TeamID      uint32 `json:"teamId"`
 	}
+
+	// GetTeamStrategyParams 获取团队策略参数
+	GetTeamStrategyParams struct {
+		TeamID     uint32 `json:"teamId"`
+		StrategyID uint32 `json:"strategyId"`
+	}
+
+	// GetTeamStrategyLevelParams 获取团队策略等级参数
+	GetTeamStrategyLevelParams struct {
+		TeamID  uint32 `json:"teamId"`
+		LevelID uint32 `json:"level"`
+	}
+
+	//GetTeamDatasourceParams  获取团队数据源信息参数
+	GetTeamDatasourceParams struct {
+		TeamID        uint32   `json:"teamId"`
+		DatasourceIds []uint32 `json:"datasourceIds"`
+	}
 )
