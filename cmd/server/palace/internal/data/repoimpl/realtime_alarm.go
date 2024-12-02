fd2a95535aed7f2472c60766ab62d001f2dbdf5e
梧桐 <1058165620@qq.com>
2024-12-02 10:51:01 +0800

    fix(server): 修复 Metric策略类型下 Level 显示错误的问题
    - 移除了 strategy_metric_test.go 中的冗余 Step 字段
    - 删除了 strategy.go 中的冗余 break 语句- 修正了 history.go 和 realtime_alarm.go 中 Metric 策略类型的 Level 显示
    - 优化了 strategy.go 中的错误处理逻辑
    
    feat: Modify Policy Structure (#176)
    
    <!--
    Thanks for sending a pull request!
    Before submitting your pull request, please make sure the checklist
    below is confirmed.
    Your pull requests will be merged after one of the collaborators
    approve.
    -->
    
    ### What type of PR is this?
    
    <!--
    Add one of the following kinds:
    /kind feat
    /kind bug
    /kind cleanup
    /kind doc
    
    Optionally add one or more of the following kinds if applicable:
    /kind api-change
    /kind deprecation
    /kind failing-test
    /kind flake
    /kind regression
    -->
    
    ### What this PR does?
    
    ### Which issue(s) this PR fixes:
    <!--
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    _If PR is about `failing-tests or flakes`, please post the related
    issues/tests in a comment and do not use `Fixes`_*
    -->
    Fixes #
    
    #### Does this PR introduce a user-facing change?
    <!--
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required:
    Enter your extended release note in the block below. If the PR requires
    additional action from users switching to the new release, include the
    string "action required".
    -->
    ```release-note
    
    ```
    
    fix(server): 修复 Metric策略类型下 Level 显示错误的问题
    
    - 移除了 strategy_metric_test.go 中的冗余 Step 字段
    - 删除了 strategy.go 中的冗余 break 语句- 修正了 history.go 和 realtime_alarm.go 中 Metric 策略类型的 Level 显示
    - 优化了 strategy.go 中的错误处理逻辑

--- cmd/server/palace/internal/data/repoimpl/realtime_alarm.go
+++ cmd/server/palace/internal/data/repoimpl/realtime_alarm.go
@@ -2,9 +2,6 @@ package repoimpl
 
 import (
 	"context"
-	"github.com/aide-family/moon/pkg/palace/model/bizmodel"
-
-	"github.com/aide-family/moon/pkg/palace/model/bizmodel"
 
 	"github.com/aide-family/moon/cmd/server/palace/internal/biz/bo"
 	"github.com/aide-family/moon/cmd/server/palace/internal/biz/repository"
@@ -13,6 +10,7 @@ import (
 	"github.com/aide-family/moon/pkg/merr"
 	"github.com/aide-family/moon/pkg/palace/model/alarmmodel"
 	"github.com/aide-family/moon/pkg/palace/model/alarmmodel/alarmquery"
+	"github.com/aide-family/moon/pkg/palace/model/bizmodel"
 	"github.com/aide-family/moon/pkg/util/types"
 	"github.com/aide-family/moon/pkg/vobj"
 
