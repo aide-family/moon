3d2f99aea941780c54c941206af5f53f837d4542
梧桐 <1058165620@qq.com>
2024-12-11 17:39:03 +0800

    test(helper): 新增 JWT 配置模拟类并设置 JWT 配置
    - 新增 JwtConfigMock 结构体和相关方法，用于模拟 JWT 配置
    - 在测试中使用 JwtConfigMock 设置 JWT 配置
    - 更新 TestNewJwtClaims 测试用例，使用新的 JWT 配置

--- pkg/helper/middleware/jwt_test.go
+++ pkg/helper/middleware/jwt_test.go
@@ -2,9 +2,35 @@ package middleware
 
 import (
 	"testing"
+	"time"
+
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
+type JwtConfigMock struct {
+	signKey string
+	expire  time.Duration
+	issuer  string
+}
+
+func (j *JwtConfigMock) GetSignKey() string {
+	return j.signKey
+}
+
+func (j *JwtConfigMock) GetExpire() *durationpb.Duration {
+	return durationpb.New(j.expire)
+}
+
+func (j *JwtConfigMock) GetIssuer() string {
+	return j.issuer
+}
+
 func TestNewJwtClaims(t *testing.T) {
+	SetJwtConfig(&JwtConfigMock{
+		signKey: "moon-sign_key",
+		expire:  time.Second * 3600,
+		issuer:  "moon-palace",
+	})
 	token, err := NewJwtClaims(&JwtBaseInfo{
 		UserID:   1,
 		TeamID:   1,
