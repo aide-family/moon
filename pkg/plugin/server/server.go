721487de1d7f8284bba51b455a8fcf385c2403a1
aide <1058165620@qq.com>
2025-05-17 21:33:35 +0800

    refactor: uncomment json.MarshalOptions in plugin server
    - Uncommented the UseEnumNumbers option in json.MarshalOptions
    - This change will emit enum values as numbers instead of strings
    - Other options (UseProtoNames and EmitUnpopulated) remain unchanged

--- pkg/plugin/server/server.go
+++ pkg/plugin/server/server.go
@@ -8,7 +8,7 @@ import (
 
 func init() {
 	json.MarshalOptions = protojson.MarshalOptions{
-		//UseEnumNumbers:  true, // Emit enum values as numbers instead of their string representation (default is string).
+		UseEnumNumbers:  true, // Emit enum values as numbers instead of their string representation (default is string).
 		UseProtoNames:   true, // Use the field names defined in the proto file as the output field names.
 		EmitUnpopulated: true, // Emit fields even if they are unset or empty.
 	}
