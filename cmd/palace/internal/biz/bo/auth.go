261fc5c247ed06ca3802e2d93f898c2bc8ba4c52
jinronga <52482860+jinronga@users.noreply.github.com>
2025-05-19 16:14:12 +0800

    feat(feishu): add feishu oauth login (#220)

--- cmd/palace/internal/biz/bo/auth.go
+++ cmd/palace/internal/biz/bo/auth.go
@@ -1,6 +1,7 @@
 package bo
 
 import (
+	"encoding/json"
 	"github.com/aide-family/moon/cmd/palace/internal/biz/vobj"
 	"github.com/aide-family/moon/cmd/palace/internal/helper/middleware"
 )
@@ -80,4 +81,68 @@ type TeamRoleItem struct {
 	ID     uint32            `json:"id"`
 	Name   string            `json:"name"`
 	Status vobj.GlobalStatus `json:"status"`
+}
+
+var _ IOAuthUser = (*FeiShuUser)(nil)
+
+// FeiShuUser 飞书用户
+type FeiShuUser struct {
+	Name            string `json:"name"`
+	EnName          string `json:"en_name"`
+	AvatarURL       string `json:"avatar_url"`
+	AvatarThumb     string `json:"avatar_thumb"`
+	AvatarMiddle    string `json:"avatar_middle"`
+	AvatarBig       string `json:"avatar_big"`
+	OpenID          string `json:"open_id"`          // 用户在当前应用中的唯一标识
+	UnionID         string `json:"union_id"`         // 用户在飞书开放平台中的唯一标识
+	Email           string `json:"email"`            // 用户邮箱
+	EnterpriseEmail string `json:"enterprise_email"` // 企业邮箱
+	ID              string `json:"user_id"`          // 用户ID（旧版字段）
+	Mobile          string `json:"mobile"`           // 手机号（带国际码）
+	TenantKey       string `json:"tenant_key"`       // 企业唯一标识
+	EmployeeNo      string `json:"employee_no"`      // 工号
+
+	userID uint32
+}
+
+func (f *FeiShuUser) String() string {
+	bs, _ := json.Marshal(f)
+	return string(bs)
+}
+
+func (f *FeiShuUser) GetOpenID() string {
+	return f.OpenID
+}
+
+func (f *FeiShuUser) GetEmail() string {
+	return f.Email
+}
+
+func (f *FeiShuUser) GetRemark() string {
+	return f.EmployeeNo
+}
+
+func (f *FeiShuUser) GetUsername() string {
+	return f.Name
+}
+
+func (f *FeiShuUser) GetNickname() string {
+	return f.EnName
+}
+
+func (f *FeiShuUser) GetAvatar() string {
+	return f.AvatarURL
+}
+
+func (f *FeiShuUser) GetAPP() vobj.OAuthAPP {
+	return vobj.OAuthAPPFeiShu
+}
+
+func (f *FeiShuUser) WithUserID(userID uint32) IOAuthUser {
+	f.userID = userID
+	return f
+}
+
+func (f *FeiShuUser) GetUserID() uint32 {
+	return f.userID
 }
