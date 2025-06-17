f6ca1a8f45381e418e6eaf6d2695f7fe59337396
aide <1058165620@qq.com>
2025-06-17 16:32:32 +0800

    feat(menu): add soft delete functionality
    - Integrate soft_delete plugin to enable soft deletion for Menu entity
    - Add DeletedAt field to Menu struct for tracking deletion timestamp
    - Update unique index to consider deleted_at column
    - Implement GetDeletedAt method for Menu struct

--- cmd/palace/internal/biz/do/system/menu.go
+++ cmd/palace/internal/biz/do/system/menu.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aide-family/moon/cmd/palace/internal/biz/do"
 	"github.com/aide-family/moon/cmd/palace/internal/biz/vobj"
+	"gorm.io/plugin/soft_delete"
 )
 
 var _ do.Menu = (*Menu)(nil)
@@ -13,18 +14,23 @@ const tableNameMenu = "sys_menus"
 
 type Menu struct {
 	do.BaseModel
-	Name          string               `gorm:"column:name;type:varchar(64);not null;uniqueIndex:idx__menu__name,priority:1;comment:menu name" json:"name"`
-	MenuPath      string               `gorm:"column:menu_path;type:varchar(255);not null;default:'';comment:menu path" json:"menuPath"`
-	MenuIcon      string               `gorm:"column:menu_icon;type:varchar(64);not null;default:'';comment:menu icon" json:"menuIcon"`
-	MenuType      vobj.MenuType        `gorm:"column:menu_type;type:tinyint(2);not null;default:0;comment:menu system type" json:"menuType"`
-	MenuCategory  vobj.MenuCategory    `gorm:"column:menu_category;type:tinyint(2);not null;default:0;comment:menu category" json:"menuCategory"`
-	ApiPath       string               `gorm:"column:api_path;type:varchar(255);not null;default:'';comment:API path" json:"apiPath"`
-	Status        vobj.GlobalStatus    `gorm:"column:status;type:tinyint(2);not null;default:0;comment:status" json:"status"`
-	ProcessType   vobj.MenuProcessType `gorm:"column:process_type;type:tinyint(2);not null;default:0;comment:process type" json:"processType"`
-	ParentID      uint32               `gorm:"column:parent_id;type:int unsigned;not null;default:0;comment:parent ID" json:"parentID"`
-	Parent        *Menu                `gorm:"foreignKey:ParentID;references:ID" json:"parent"`
-	RelyOnBrother bool                 `gorm:"column:rely_on_brother;type:tinyint(1);not null;default:0;comment:whether to rely on sibling node" json:"relyOnBrother"`
-	Sort          uint32               `gorm:"column:sort;type:int unsigned;not null;default:0;comment:sort" json:"sort"`
+	DeletedAt     soft_delete.DeletedAt `gorm:"column:deleted_at;type:bigint;not null;default:0;uniqueIndex:idx__menu__name__deleted_at,priority:2;" json:"deleted_at,omitempty"`
+	Name          string                `gorm:"column:name;type:varchar(64);not null;uniqueIndex:idx__menu__name__deleted_at,priority:1;comment:menu name" json:"name"`
+	MenuPath      string                `gorm:"column:menu_path;type:varchar(255);not null;default:'';comment:menu path" json:"menuPath"`
+	MenuIcon      string                `gorm:"column:menu_icon;type:varchar(64);not null;default:'';comment:menu icon" json:"menuIcon"`
+	MenuType      vobj.MenuType         `gorm:"column:menu_type;type:tinyint(2);not null;default:0;comment:menu system type" json:"menuType"`
+	MenuCategory  vobj.MenuCategory     `gorm:"column:menu_category;type:tinyint(2);not null;default:0;comment:menu category" json:"menuCategory"`
+	ApiPath       string                `gorm:"column:api_path;type:varchar(255);not null;default:'';comment:API path" json:"apiPath"`
+	Status        vobj.GlobalStatus     `gorm:"column:status;type:tinyint(2);not null;default:0;comment:status" json:"status"`
+	ProcessType   vobj.MenuProcessType  `gorm:"column:process_type;type:tinyint(2);not null;default:0;comment:process type" json:"processType"`
+	ParentID      uint32                `gorm:"column:parent_id;type:int unsigned;not null;default:0;comment:parent ID" json:"parentID"`
+	Parent        *Menu                 `gorm:"foreignKey:ParentID;references:ID" json:"parent"`
+	RelyOnBrother bool                  `gorm:"column:rely_on_brother;type:tinyint(1);not null;default:0;comment:whether to rely on sibling node" json:"relyOnBrother"`
+	Sort          uint32                `gorm:"column:sort;type:int unsigned;not null;default:0;comment:sort" json:"sort"`
+}
+
+func (u *Menu) GetDeletedAt() soft_delete.DeletedAt {
+	return u.DeletedAt
 }
 
 func (u *Menu) GetName() string {
